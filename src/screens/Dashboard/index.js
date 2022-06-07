3fd5b2c3f6759ebc3d6396328a6a10f9e7bf355f
Dzun <nurroinz@gmail.com>
2022-06-07 22:31:22 +0700

    init list pokemon

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {FlatList, StyleSheet, Text, View, Platform} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {PokemonCard} from '../../components';
+import {fetchPokemons} from '../../store/actions';
+import {COLORS, FONTS} from '../../themes';
 
 export default function Dashboard() {
+  const dispatch = useDispatch();
+  const pokemons = useSelector(state => state.PokeReducer.pokemons);
+  // const loading = useSelector(state => state.PokeReducer.loading);
+  // const next = useSelector(state => state.PokeReducer.next);
+  // const back = useSelector(state => state.PokeReducer.back);
+
+  useEffect(() => {
+    dispatch(fetchPokemons());
+    console.log("Helloooooooooooooooooooooo pokemon", pokemons);
+  }, []);
+
   return (
-    <View>
-      <Text>Dashboard</Text>
+    <View style={styles.container}>
+      <Text style={styles.text_title}>Pokédex</Text>
+      <FlatList
+        data={pokemons}
+        numColumns={2}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={pokemon => String(pokemon.id)}
+        renderItem={({item}) => <PokemonCard pokemon={item} />}
+        contentContainerStyle={styles.flatListContentContainer}
+      />
     </View>
   );
 }
 
-// const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FBFB',
+    // padding: 10,
+  },
+  text_title: {
+    ...FONTS.largeTitle,
+    color: COLORS.black,
+    margin: 10,
+    marginBottom: 15,
+  },
+  flatListContentContainer: {
+    paddingHorizontal: 5,
+    marginTop: Platform.OS === "android" ? 5 : 0,
+},
+});
